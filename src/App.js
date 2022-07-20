75d8e2a1390267c38ff0abc200c9874f6b566532
Javiera <jbkc_10@hotmail.com>
2022-07-20 15:42:45 -0400

    router y vistas

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
-import logo from './logo.svg';
+import * as React from "react";
+import { Routes, Route, Link } from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+        <Route path="notes" element={<Notes />} />
+      </Routes>
     </div>
+  );
+}
+
+function Home() {
+  return (
+    <>
+      <main>
+      <h1>Welcome to Cat-notes!</h1>
+        <p>Login with Google</p>
+        <p><Link className="links" to="/login">Login with email</Link></p>
+        <p><Link className="links" to="/register">Register</Link></p>
+      </main>
+    </>
+  );
+}
+
+function Login() {
+  return (
+    <>
+      <main>
+        <h1>Enter your data to log in</h1>
+        <p>Email:</p>
+        <p>Password:</p>
+        <p><Link className="links" to="/">↩️</Link></p>
+      </main>
+      {/* <nav>
+        <Link to="/">Home</Link>
+      </nav> */}
+    </>
+  );
+}
+function Register() {
+  return (
+    <>
+      <main>
+      <h1>Enter your data to register</h1>
+        <p>User:</p>
+        <p>Email:</p>
+        <p>Password:</p>
+        <p><Link className="links" to="/">↩️</Link></p>
+      </main>
+    </>
+  );
+}
+function Notes() {
+  return (
+    <>
+      <main>
+        <h2>Who are we?</h2>
+        <p>
+          That feels like an existential question, don't you
+          think?
+        </p>
+      </main>
+    </>
   );
 }
 
