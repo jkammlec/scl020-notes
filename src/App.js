cdfe15665acd82d6b91e2900c2ddb1cd3c584aeb
Javiera <jbkc_10@hotmail.com>
2022-08-02 15:55:24 -0400

    configuracion inicial register

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import * as React from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import './App.css';
-import { getAuth, signInWithRedirect } from "firebase/auth";
 import { Home } from "./components/Home.js"
 import { Login } from "./components/Login.js"
 import { Register } from "./components/Register.js"
 import { Notes } from "./components/Notes.js"
+import { AuthProvider } from "./context/authContext";
 
 function App() {
   return (
+      <AuthProvider>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="login" element={<Login />} />
         <Route path="register" element={<Register />} />
         <Route path="notes" element={<Notes />} />
       </Routes>
+      </AuthProvider>
   );
 }
 
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,17 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export function Login() {
+  const [user, setUser] = useState({
+    email: '',
+    password: ''
+  })
     return (
       <>
         <main className="mainBody">
           <div className="App">
           <h1>Enter your data to log in</h1>
-          <p>Email: <input id="mail" type="text" placeholder="Email that you use"></input></p>
+          <p>Email: <input id="mail" type="email" placeholder="Email that you use"></input></p>
           <p>Password: <input id="pass" type="password" placeholder="Password "></input></p>
           <p><Link className="links" to="/">↩️</Link> <Link className="links" to="/notes"><button id="btn_login" type="submit">Login</button></Link></p>
           </div>
