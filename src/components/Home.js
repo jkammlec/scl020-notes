9ce14f43c31a4a24402a124333f9079c5a6cb736
Javiera <jbkc_10@hotmail.com>
2022-08-03 17:28:28 -0400

    logout y login con Google

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,19 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../context/authContext";
 
 export function Home() {
-
+  const { loginWithGoogle } = useAuth()  
+  const navigate = useNavigate()
+  const handleGoogleSignin = async () => {
+      await loginWithGoogle();
+      navigate ("/notes")
+    }
     return (
       <>
         <main className="mainBody">
           <div className="App">
           <h1>Welcome to Cat-notes!</h1>
-          <p>Login with Google</p>
+          <p>Login with <button className="google" onClick={handleGoogleSignin} ></button></p>
           <p><Link className="links" to="/login">Login with email</Link></p>
           <p><Link className="links" to="/register">Register</Link></p>
           </div>
--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,10 +1,24 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../context/authContext.js";
 
 export function Notes() {
+  const {user, logout} = useAuth()
+  const navigate = useNavigate() //para "navegar" entre las rutas de la app
+
+  const handleLogout = async () => { //como viene de signOut, es asíncrona
+    try {
+      await logout()
+      navigate ("/")
+    } catch (error) {
+      console.error(error);
+    } 
+  }
+  console.log(user)
     return (
       <>
         <main className="notesView">
           <div className="head">
+            <button className="btnLogout" onClick={handleLogout}>Logout</button>
             <h1> Welcome to your cat-notes</h1>
           </div>
         </main>
